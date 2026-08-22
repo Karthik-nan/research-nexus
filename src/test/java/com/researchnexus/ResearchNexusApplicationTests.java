9c6ff174cdcd60b8d9975c327c7c4c9644dcca8f
Nandagiri Karthik <nandagirikarthik9@gmail.com>
2026-08-22 15:43:11 +0530

    Fix Spring Boot tests for CI

--- src/test/java/com/researchnexus/ResearchNexusApplicationTests.java
+++ src/test/java/com/researchnexus/ResearchNexusApplicationTests.java
@@ -1,4 +1,4 @@
-package com.openresearchnexus.research_nexus;
+package com.researchnexus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ResearchNexusApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
